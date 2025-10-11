7afc4c62cece5c10d96db53542b7623eef1175e8
Jannat-Dawabsheh <S11923966@stu.najah.edu>
2025-10-11 18:11:06 +0300

    first commit

---
+++ kaShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using kaShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace kaShop.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.;Database=Ka_Shop;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+              new Category { Id=1,Name="Mobile"},
+              new Category { Id = 2, Name = "Tablets" },
+              new Category { Id = 3, Name = "Laptops" }
+            );
+        }
+    }
+}
---
+++ kaShop/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace kaShop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        
+        public decimal Price { get; set; }
+        public int Rate { get; set; }
+        public int Quantity { get; set; }
+
+        public string Image { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
